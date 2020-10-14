3acc7f7e3461a89992d45ff5a39cc7ad7577e366
harisumaid27 <harisumaid27@gmail.com>
2020-10-14 13:44:46 +0530

    implement navigation menu

--- components/firstPage/landing_section.js
+++ components/firstPage/landing_section.js
@@ -1,4 +1,4 @@
-import {Image,Dimmer,Button, Icon} from 'semantic-ui-react'
+import {Image,Dimmer,Button, Icon, Header} from 'semantic-ui-react'
 import styles from '../../styles/components/homePage/LandingSection.module.css'
 import Link from 'next/link'
 import {useState} from 'react'
@@ -9,10 +9,68 @@ export default function LandingSection(){
     const menuClick = ()=>{setActiveMenu(!activeMenu)}
     return(
         <>
+        <Dimmer.Dimmable blurring dimmed={activeMenu} >
+        <Dimmer active={activeMenu} page onClickOutside={menuClick} blurring >
+            <Button basic inverted size='large' id={styles.backInMenu} onClick={menuClick} >
+                <Icon name='arrow left' ></Icon>
+                Back
+            </Button>
+            <div className={styles.menuInDimmer} >
+            <div className={styles.firstDivMenu} >
+                <Link href="/" >
+                    <a><h1 onClick={()=>{setActiveMenu(false)}}  >Home</h1></a>
+                </Link>
+                <Link href="/about" >
+                    <a><h1>About</h1></a>
+                </Link>
+                <Link href="/gallery" >
+                    <a><h1>Gallery</h1></a>
+                </Link>
+                <Link href="/contact" >
+                    <a><h1>Contact us</h1></a>
+                </Link>
+            </div>
+            <div className={styles.menuInMobileOrTablet} >
+                <Link href="/" >
+                    <h1 onClick={()=>{setActiveMenu(false)}} >Home</h1>
+                </Link>
+                <Link href="/about" >
+                    <h1>About</h1>
+                </Link>
+                <Link href="/gallery" >
+                    <h1>Gallery</h1>
+                </Link>
+                <Link href="/contact" >
+                    <h1>Contact us</h1>
+                </Link>
+                <Link href="/news" >
+                    <h1>In the news</h1>
+                </Link>
+                <Link href="/achievement" >
+                    <h1>Achievements</h1>
+                </Link>
+                <Link href="/office" >
+                    <h1>Office Bearers</h1>
+                </Link>
+            </div>
+            <div className={styles.secondDivMenu} >
+                <Link href="/news" >
+                    <a><h1>In the news</h1></a>
+                </Link>
+                <Link href="/achievement" >
+                    <a><h1>Achievements</h1></a>
+                </Link>
+                <Link href="/office" >
+                    <a><h1>Office Bearers</h1></a>
+                </Link>
+            </div>
+            
+            </div>
+        </Dimmer>
          <div>
             <div className={styles.firstDiv} >
                 <Icon link name={activeMenu?'close':'bars'} onClick={menuClick} id={styles.topDivMenu} size='huge' ></Icon>
-                <p className={styles.logo} >KNPSS</p>
+                <Link href="/" ><a className={styles.logo} >KNPSS</a></Link>
                 <h1 className={styles.title} >Kalinganagar Paribesh Surakshya Samiti</h1>
                 <div className={styles.centerDescriptionContainer} >
                     <div className={styles.centerDescription} >
@@ -38,6 +96,7 @@ export default function LandingSection(){
                     <Button color='yellow' id={styles.helpUsDonate} size='huge' >Join us</Button>
                 </div>
         </div>
+        </Dimmer.Dimmable>
         </>
         )
 }
