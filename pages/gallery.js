f8160a8c237ca847bcf1269aba103b929c5b3e67
harisumaid27 <harisumaid27@gmail.com>
2020-10-25 13:34:09 +0530

    gallery route implemented

--- pages/gallery.js
+++ pages/gallery.js
@@ -1,9 +1,42 @@
-export default function Gallery(params) {
-    return(
-        <div>
-            <h1>
-                Gallery Section
-            </h1>
-        </div>
-    )
+import { Table } from "semantic-ui-react";
+import { fetchAllGallery } from "../lib/fetchForGallery";
+import Link from "next/link";
+
+export default function Gallery({ Gallery }) {
+  return (
+    <div>
+      <h1>Gallery Section</h1>
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Id</Table.HeaderCell>
+            <Table.HeaderCell>Heading</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          {Gallery.map((gallery) => {
+              return (
+            <Table.Row key={gallery.type+gallery._id}>
+              <Table.Cell>{gallery._id}</Table.Cell>
+              <Table.Cell>
+                <Link href={`/${gallery.type}/${gallery._id}`}>
+                  <a>{gallery.heading}</a>
+                </Link>
+              </Table.Cell>
+            </Table.Row>
+          )})}
+        </Table.Body>
+      </Table>
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  const Gallery = JSON.stringify(await fetchAllGallery());
+  return {
+    props: {
+      Gallery: JSON.parse(Gallery),
+    },
+    revalidate: 1,
+  };
 }
