224d2a2f9c83c5ef57df1ebe08b75d51d9f87053
harisumaid27 <harisumaid27@gmail.com>
2020-10-30 21:48:51 +0530

    blog component implemented

--- pages/achievement/[id].js
+++ pages/achievement/[id].js
@@ -1,52 +1,40 @@
-import { fetchIdList,fetchForId } from '../../lib/fetchForAchievement'
-import { useRouter } from 'next/router'
+import { fetchIdList, fetchForId } from "../../lib/fetchForAchievement";
+import { useRouter } from "next/router";
+import Blog from "../../components/blog";
+import { Dimmer, Header } from "semantic-ui-react";
 
 export default function Post({ achievement }) {
   const router = useRouter();
-  if(router.isFallback){
-    return <div>Loading ...</div>
+  if (router.isFallback) {
+    return <div>
+      <Dimmer page >
+        <Header>
+          Content Loading Please Wait!!
+        </Header>
+      </Dimmer>
+    </div>;
   }
 
   return (
     <div>
-      <ul>
-        <li key={achievement._id} >{achievement._id}</li>
-        <li key={achievement.heading}>{achievement.heading}</li>
-        <li key={achievement.content}>{achievement.content}</li>
-        <li key='images'>
-          Images:-
-          <ul>
-            {achievement.imagesPath.map((path) => {
-              return(<li key={path} >{path}</li>);
-            })}
-          </ul>
-        </li>
-        <li key='pdfs' >
-            Pdfs:- 
-            <ul>
-                {achievement.pdfsPath.map((path)=>{
-                    return(<li key={path} >{path}</li>)
-                })}
-            </ul>
-        </li>
-      </ul>
+      <Blog post={achievement} />
     </div>
   );
 }
 
-export async function getStaticPaths(){
-    const paths = await fetchIdList();
-    return {
-        paths,
-        fallback: true,
-    }
+export async function getStaticPaths() {
+  const paths = await fetchIdList();
+  return {
+    paths,
+    fallback: true,
+  };
 }
 
-export async function getStaticProps({params}){
-    const achievement = JSON.parse(await fetchForId(params.id));
-    return {
-        props:{
-            achievement:achievement[0],
-        }
-    }
+export async function getStaticProps({ params }) {
+  const achievement = JSON.parse(await fetchForId(params.id));
+  return {
+    props: {
+      achievement: achievement[0],
+    },
+  };
 }
--- pages/news/[id].js
+++ pages/news/[id].js
@@ -1,50 +1,32 @@
-import { fetchIdList,fetchForId } from '../../lib/fetchForNews'
-import { useRouter } from 'next/router'
+import { fetchIdList, fetchForId } from "../../lib/fetchForNews";
+import { useRouter } from "next/router";
+import Blog from "../../components/blog";
+
 export default function Post({ news }) {
   const router = useRouter();
   if (router.isFallback) {
-    return <div>...Loading</div>  
+    return <div>...Loading</div>;
   }
   return (
     <div>
-      <ul>
-        <li key={news._id} >{news._id}</li>
-        <li key={news.heading}>{news.heading}</li>
-        <li key={news.content}>{news.content}</li>
-        <li key='images'>
-          Images:-
-          <ul>
-            {news.imagesPath.map((path) => {
-              return(<li key={path} >{path}</li>);
-            })}
-          </ul>
-        </li>
-        <li key='pdfs' >
-            Pdfs:- 
-            <ul>
-                {news.pdfsPath.map((path)=>{
-                    return(<li key={path} >{path}</li>)
-                })}
-            </ul>
-        </li>
-      </ul>
+      <Blog post={news} />
     </div>
   );
 }
 
-export async function getStaticPaths(){
-    const paths = await fetchIdList();
-    return {
-        paths,
-        fallback: true,
-    }
+export async function getStaticPaths() {
+  const paths = await fetchIdList();
+  return {
+    paths,
+    fallback: true,
+  };
 }
 
-export async function getStaticProps({params}){
-    const news = JSON.parse(await fetchForId(params.id));
-    return {
-        props:{
-            news:news[0],
-        }
-    }
+export async function getStaticProps({ params }) {
+  const news = JSON.parse(await fetchForId(params.id));
+  return {
+    props: {
+      news: news[0],
+    },
+  };
 }
