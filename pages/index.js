2a044da3557738a440b702d1b35844e6cd871ccc
harisumaid27 <harisumaid27@gmail.com>
2020-11-01 08:48:23 +0530

    carousel fix

--- pages/index.js
+++ pages/index.js
@@ -1,29 +1,32 @@
-import Head from 'next/head'
-import LandingSection from '../components/firstPage/landing_section'
-import SecondSection from '../components/firstPage/second_section'
-import ThirdSection from '../components/firstPage/third_section'
-import {datajsoning} from './api/jsonForHome'
+import Head from "next/head";
+import LandingSection from "../components/firstPage/landing_section";
+import SecondSection from "../components/firstPage/second_section";
+import ThirdSection from "../components/firstPage/third_section";
+import { datajsoning } from "./api/jsonForHome";
+import { fetchAllGallery } from "../lib/fetchForGallery";
 
-export default function Home({data}) {
-    return(
+export default function Home({ Gallery,data }) {
+  return (
     <div>
-        <Head>
-            <title>KNPSS</title>
-        </Head>
-        <LandingSection/>
-        <SecondSection/>
-        <ThirdSection props={data.data}/>
+      <Head>
+        <title>KNPSS</title>
+      </Head>
+      <LandingSection />
+      <SecondSection Gallery={Gallery} />
+      <ThirdSection props={data.data} />
     </div>
-    );
+  );
 }
 
-export async function getServerSideProps() {
-    const res =  datajsoning();
-    const data = JSON.parse(res);
-    return {
-        props : {
-            data,
-        }
-    }
+export async function getStaticProps() {
+  const res = datajsoning();
+  const data = JSON.parse(res);
+  const Gallery = JSON.parse(await fetchAllGallery());
+  return {
+    props: {
+      Gallery,
+      data,
+    },
+    revalidate: 1,
+  };
 }
-
